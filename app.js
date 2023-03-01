a70e538767d5a5fca63d723c305eca978725f001
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-02 00:22:55 +0530

    Server asset files statically

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const path = require("node:path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "images"))); multiple static middlewares are possible
+// all static middlewares stop when file is found
+
 app.set("easy-app-root-path", __dirname);
 
 app.use(cors()); // ignore
